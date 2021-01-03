4f2291be1c3ac5381cd2bc156895770f35f198bb
Haroun Serang <haroun@zenput.com>
2021-01-03 13:58:47 -0800

    Add eslint with basic config

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: ['plugin:react/recommended', 'standard'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        indent: ['error', 4],
+        semi: ['error', 'always'],
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': ['error']
+    }
+};
